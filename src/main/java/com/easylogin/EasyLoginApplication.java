ff3228352970eea0eaf961431c4faceae30d3dda
alamin-7 <alamin188914@gmail.com>
2024-08-10 16:49:17 +0600

    spring boot structure added

---
+++ src/main/java/com/easylogin/EasyLoginApplication.java
@@ -0,0 +1,13 @@
+package com.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easylogin/EasyLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
